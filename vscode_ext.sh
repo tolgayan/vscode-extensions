a6bec8f7fe93f229b8239f1f71f1c4bc4d87a98c
E. Tolga Ayan <33233561+tolgayan@users.noreply.github.com>
2021-07-26 15:55:03 +0300

    Create vscode_ext.sh

---
+++ vscode_ext.sh
@@ -0,0 +1,9 @@
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension formulahendry.code-runner
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension waderyan.gitblame
+code --install-extension njpwerner.autodocstring
+code --install-extension thomascsd.vscode-readme-pattern
