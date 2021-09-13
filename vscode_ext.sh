6aaf711b67edb29d1f6762eb84e5dec21b6e3d58
E. Tolga Ayan <33233561+tolgayan@users.noreply.github.com>
2021-09-13 09:25:07 +0300

    Update vscode_ext.sh

--- vscode_ext.sh
+++ vscode_ext.sh
@@ -7,4 +7,4 @@ code --install-extension GrapeCity.gc-excelviewer
 code --install-extension waderyan.gitblame
 code --install-extension njpwerner.autodocstring
 code --install-extension thomascsd.vscode-readme-pattern
-code --ınstall-extension ritwickdey.liveserver
+code --install-extension ritwickdey.liveserver
